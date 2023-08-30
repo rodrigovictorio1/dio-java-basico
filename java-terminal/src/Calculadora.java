6470ac49f004151f5d0f9bb92ee781094a6c0d44
Rodrigo Victorio <rodrigovictorio.com@gmail.com>
2023-08-30 00:58:26 -0300

    Adiconando arquivos ao projeto

---
+++ java-terminal/src/Calculadora.java
@@ -0,0 +1,22 @@
+/**
+ * <h1>Calculadora</h1>
+ * A Calculadora realiza operações matemáticas entre número inteiros
+ * <p>
+ * <b>Note:</b> Leia atentamente a documentação destas classes para desfrutar dos recursos oferecidos pelo autor
+ * 
+ * @author Rodrigo Victório
+ * @version 1.0
+ * @since 01/01/2023
+ */
+
+public class Calculadora {
+    /**
+     * Este método é utilizado para somar dois números inteiros
+     * @param numeroUm este é o primeiro parâmetro do método
+     * @param numeroDois este é o segundo parâmetro do método
+     * @return int o resultado deste mérodo é a soma dos dois números.
+     */
+    public int somar(int numeroUm, int numeroDois) {
+        return numeroUm + numeroDois;
+    }
+}
---
+++ java-terminal/src/Comentarios.java
@@ -0,0 +1,45 @@
+public class Comentarios {
+    public static void main(String[] args) {
+        // Olá, eu sou um comentário em uma única linha
+
+        /**
+         * Olá,
+         * Eu sou um comentário
+         * que posso ser mais detalhado
+         * quando necessário
+         */
+    }
+    /**
+     * Estas duas estrelinhas acima
+     * é para identificar que você
+     * pretende elaborar um comentário
+     * a nível de documentação.
+     * Que incrível !!!
+s     */
+
+    public void metodo() {
+
+    }
+
+    /**
+     * Este método foi elaborado as pressas
+     * por isso eu abreviei o nome das variáveis
+     * mas olha, ele tem a finalidade de somar ou 
+     * multiplicar dois números.
+     * @param n
+     * @param x
+     * @param m
+     * @return
+     */
+    public int somaMultiplica(int n, int x, String m) {
+        int r = 0; // r é igual ao resultado
+
+        if (m == "M") { // M = multiplicação
+            r = n * x;
+        } else {
+            // se não soma mesmo
+            r = n + x;
+        }
+        return r;
+    }
+}
