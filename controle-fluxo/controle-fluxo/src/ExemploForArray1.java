17a89dd72778a579206ba741b8f89e0de8765206
rodrigovictorio <rodrigovictorio.com@gmail.com>
2023-09-01 17:54:30 -0300

    6. Estruturas de Repetição em Java

---
+++ controle-fluxo/controle-fluxo/src/ExemploForArray1.java
@@ -0,0 +1,14 @@
+public class ExemploForArray1 {
+    public static void main(String[] args) {
+        String alunos[] = {
+            "FELIPE",
+            "JONAS",
+            "JULIA",
+            "MARCOS"
+        };
+
+        for(int x = 0; x < alunos.length; x++) {
+            System.out.println("O aluno no índice x = " + x + " é " + alunos[x]);
+        }
+    }
+}
